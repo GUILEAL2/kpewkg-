dca5134abf3edfabf9a71ce11058eccc87231c3f
GUILEAL2 <143729840+GUILEAL2@users.noreply.github.com>
2023-11-23 11:10:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new swiper(".myswiper",{
+    cssmode:true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+     el: ".swiper-pagination",
+    
+    },
+    mousewheel: true,
+    Keyboard: true,
+    });
